1284f3b267444f648851136d75119e771a1bd32f
pulsen <hunag123qwe@126.com>
2015-06-25 17:20:26 +0800

    binding 去除

--- lib/dface_basic/shop.rb
+++ lib/dface_basic/shop.rb
@@ -2,7 +2,6 @@ module DfaceBasic
       class Shop < DfaceBasic::Init
             def self.find_by(options)
                   begin
-                        binding.pry
                         url = "#{DfaceBasic::Configure::HOST}/shop/basic?#{options.to_param}"
                         self.new(DfaceBasic.get(url))
                   rescue 
